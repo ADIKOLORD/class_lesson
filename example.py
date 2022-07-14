a6cec792d48c2ef797db68b963bf6ee1cc97aa80
Kyrgyzbaev ADILET <98408321+ADIKOLORD@users.noreply.github.com>
2022-07-14 19:07:37 +0600

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 class Samsung:
-aafffaaaafdaffafddfddfffsdfddd
+aafffaaaafdaffafddfddfffsdfddds
     def __init__(self, color: str, memory: int, battery: int):
         '''
         Обязательные аргументы при создании Экземпляра
